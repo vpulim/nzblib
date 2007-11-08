fdaadba7658de164ccdb3bcba5b446eea2bd4bf2
Michael van Tellingen <michaelvantellingen@gmail.com>
2007-11-08 14:11:42 +0000

    remove more cleanable files..

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,5 @@
-rm -f config.* missing install-sh ltmain.sh aclocal.m4 stamp-h1 Makefile configure
+rm -f config.* missing install-sh ltmain.sh aclocal.m4 stamp-h1 Makefile configure libtool depcomp Makefile.in
 rm -rf autom4te.cache
-
+rm -rf .libs
+rm -rf .deps
 
